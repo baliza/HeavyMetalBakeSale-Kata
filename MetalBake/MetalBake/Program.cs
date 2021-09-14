577b3ba448e995c373cc5b02d51208784b00715c
raul cabrejas <raul@desaprendiendo.net>
2021-09-14 14:06:15 +0200

    hello world

---
+++ MetalBake/MetalBake/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MetalBake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

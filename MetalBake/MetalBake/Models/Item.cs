e7443971688872b32b3e9f2e4a3c9640a355a81f
NdeHaro <ndeharo@gteam.es>
2021-09-14 20:06:20 +0200

    Basic Structure

---
+++ MetalBake/MetalBake/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetalBake.Models
+{
+    public class Item
+    {
+        public char Code;
+        public string Name;
+        public double Price;
+        public int Amount;
+
+
+        public Item(char code,string name, double price, int amount)
+        {
+            Code = code;
+            Name = name;
+            Price = price;
+            Amount = amount;
+        }
+    }
+}
---
+++ MetalBake/MetalBake/Services/InventoryService.cs
@@ -0,0 +1,19 @@
+﻿using MetalBake.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetalBake.Services
+{
+    class InventoryService
+    {
+        public List<Item> ItemList;
+
+        public InventoryService(List<Item> itemList)
+        {
+            ItemList = itemList;
+        }
+
+
+    }
+}

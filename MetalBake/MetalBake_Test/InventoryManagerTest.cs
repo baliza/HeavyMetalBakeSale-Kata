11a62bee4761cbd24cf41c77d21a282ccfccb772
Pablo Viturro <pviturro@nettrim.es>
2021-09-15 21:41:59 +0200

    Mejores Tests de InventoryManagerService y shopping cart

--- MetalBake/MetalBake_Test/InventoryManagerTest.cs
+++ MetalBake/MetalBake_Test/InventoryManagerTest.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MetalBake.services;
+using MetalBake.models;
+using System;
 
 namespace MetalBake_Test
 {
@@ -7,17 +9,34 @@ namespace MetalBake_Test
     public class InventoryManagerTest
     {
         [TestMethod]
-        public void PriceCount_B_C_W_Test()
+        public void PurchaseData_B_C_W_Price_Test()
         {
             InventoryManagerService ims = new InventoryManagerService();
-            Assert.AreEqual(3.50m, ims.PurchaseData("B,C,W"));
+            ShoppingCart shoppingCart = ims.PurchaseData("B,C,W");
+            Assert.AreEqual(3.50m, shoppingCart.totalPrice);
         }
 
         [TestMethod]
-        public void PriceCount_Non_Registered_Product_Test()
+        public void PurchaseData_Non_Registered_Product_Total_Price_Test()
         {
             InventoryManagerService ims = new InventoryManagerService();
-            Assert.AreEqual(0, ims.PurchaseData("A"));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => ims.PurchaseData("A"));
+        }
+
+        [TestMethod]
+        public void PurchaseData_B_C_W_Item_List_Test()
+        {
+            InventoryManagerService ims = new InventoryManagerService();
+            ShoppingCart shoppingCart = ims.PurchaseData("B,C,W");
+            Assert.AreEqual("B,C,W", shoppingCart.itemList);
+        }
+
+        [TestMethod]
+        public void PurchaseData_Invalid_Chars_Item_List_Test()
+        {
+            InventoryManagerService ims = new InventoryManagerService();
+            ShoppingCart shoppingCart = ims.PurchaseData("AAAAAB,M,W,,,XXXX");
+            Assert.AreEqual("B,M,W", shoppingCart.itemList);
         }
 
 

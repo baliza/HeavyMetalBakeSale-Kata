676040dbb236ee6aeeee536f13d7aa2fc5865742
NdeHaro <ndeharo@gteam.es>
2021-09-16 11:04:44 +0200

    New test

--- MetalBake/MetalBake.Test/InventoryTest.cs
+++ MetalBake/MetalBake.Test/InventoryTest.cs
@@ -1,4 +1,7 @@
+using MetalBake.Models;
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MetalBake.Test
 {
@@ -10,9 +13,18 @@ namespace MetalBake.Test
         }
 
         [Test]
-        public void Test1()
+        public void Test_Inventory_Created()
         {
-            Assert.Pass();
+            Inventory inventory = new Inventory();
+            List<Item> itemList = new List<Item>();
+            itemList.Add(new Item('C', "Cake", 10));
+            itemList.Add(new Item('W', "Water", 7));
+            itemList.Add(new Item('B', "Brownie", 21));
+            itemList.Add(new Item('M', "Muffin", 2));
+            for(int i = 0; i < itemList.Count; i++)
+            {
+                Assert.AreEqual(itemList[i].ToString().Trim(), inventory.ItemList[i].ToString().Trim());
+            }
         }
     }
 }

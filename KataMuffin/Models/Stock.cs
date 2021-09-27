adc084f37c14fcd0c2abedb49498248b94aa14b8
JavierSG <jsanudo@gteam.es>
2021-09-27 09:14:57 +0200

    1st commit

---
+++ KataMuffin/Models/Stock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KataMuffin.Models
+{
+    public class Stock
+    {
+        public string Id;
+        public string Name;
+        public int Amount;
+
+        public Stock(string id, string name, int amount)
+        {
+            Id = id;
+            Name = name;
+            Amount = amount;
+        }
+    }
+}
